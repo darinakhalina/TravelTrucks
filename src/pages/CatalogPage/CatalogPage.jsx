afb4a3216858492827bd2b96b186dbf892173806
Darina Khalina <darina.khalina@namecheap.com>
2024-10-26 14:51:42 +0300

    add CampersList component

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,21 +1,16 @@
 import clsx from 'clsx';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { fetchCampers } from '../../redux/campersOps';
 import { clearItemsState, resetCurrentPage, setCurrentPage } from '../../redux/campersSlice';
-import { selectCampers, selectFavorites } from '../../redux/selectors';
 import { clearFilters } from '../../redux/filtersSlice';
-import { addFavorite, removeFavorite } from '../../redux/favoritesSlice';
 import CatalogFiltersForm from '../../components/CatalogFiltersForm/CatalogFiltersForm';
 import CampersLoadMoreButton from '../../components/CampersLoadMoreButton/CampersLoadMoreButton';
+import CampersList from '../../components/CampersList/CampersList';
 import css from './CatalogPage.module.css';
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
-  const campers = useSelector(selectCampers);
-  const favorites = useSelector(selectFavorites);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCampersLocal = async () => {
@@ -31,35 +26,13 @@ const CatalogPage = () => {
     };
   }, [dispatch]);
 
-  const handleNavigation = id => {
-    navigate(`/catalog/${id}`);
-  };
-
-  const handleFavoriteClick = id => {
-    // move to component and get from store by id
-    const isFavorite = favorites?.some(favoriteId => favoriteId === id);
-    if (isFavorite) {
-      dispatch(removeFavorite(id));
-    } else {
-      dispatch(addFavorite(id));
-    }
-  };
-
   return (
     <div className={clsx('container', css['catalog-page-container'])}>
       <div className={css['catalog-page-filters-holder']}>
         <CatalogFiltersForm />
       </div>
       <div className={css['catalog-page-campers-list-holder']}>
-        {campers.map(item => (
-          <div key={item.id}>
-            <button onClick={() => handleFavoriteClick(item.id)}>
-              {favorites?.some(favoriteId => favoriteId === item.id) ? 'YES - fav' : 'NO'}
-            </button>
-            {item.name} - {item.id}
-            <button onClick={() => handleNavigation(item.id)}>Show more</button>
-          </div>
-        ))}
+        <CampersList />
         <CampersLoadMoreButton />
       </div>
     </div>
